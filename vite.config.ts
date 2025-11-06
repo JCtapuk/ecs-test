c2be58aa74a2734a8048dbb3864aa08f85d3bb44
JCtapuk <jctapuk@outlook.com>
2025-11-06 19:26:11 +0300

    add config game

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,19 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/ecs-test/", // Обязательно! Замени на имя репозитория
+  base: "/ecs-test/",
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(process.cwd(), "index.html"),
+        game: resolve(process.cwd(), "game.html"),
+      },
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+      },
+    },
+  },
 });
